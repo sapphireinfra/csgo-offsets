e513dce7f8ca9e73975dc9cd1a1d1d2f1c722b61
SapphireInfra <dendioornndio@gmail.com>
2026-06-01 21:36:29 +0530

    Create offsets.h

---
+++ offsets.h
@@ -0,0 +1,7 @@
+constexpr std::ptrdiff_t dwEntityList                         = 0x21D6878;
+constexpr std::ptrdiff_t dwLocalPlayerController              = 0x231D830;
+constexpr std::ptrdiff_t dwLocalPlayerPawn                    = 0x231D830;
+constexpr std::ptrdiff_t dwViewMatrix                         = 0x2343AB0;
+constexpr std::ptrdiff_t dwViewAngles                         = 0x2021A50;
+constexpr std::ptrdiff_t dwGlobalVars                         = 0x205E6D0;
+constexpr std::ptrdiff_t dwCSGOInput                          = 0x1FD6AA0;
